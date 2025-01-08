0fb25247b6c8ceead33bb8896bc9b4ba2736b6cd
Matheo RIO <rio.matheo.37@gmail.com>
2025-01-08 15:31:57 +0100

    Initialisation projet

---
+++ src/main/java/fr/supmap/supmapapi/SupmapApiApplication.java
@@ -0,0 +1,13 @@
+package fr.supmap.supmapapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupmapApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupmapApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/supmap/supmapapi/SupmapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.supmap.supmapapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupmapApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
