0992abf3a650d58f27f6e39d545dce98b3cdc160
Matheo RIO <rio.matheo.37@gmail.com>
2025-02-11 14:02:48 +0100

    add mode transport

--- src/main/java/fr/supmap/supmapapi/controller/impl/DirectionControllerImpl.java
+++ src/main/java/fr/supmap/supmapapi/controller/impl/DirectionControllerImpl.java
@@ -1,11 +1,13 @@
 package fr.supmap.supmapapi.controller.impl;
 
 import fr.supmap.supmapapi.controller.DirectionController;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @RestController
 public class DirectionControllerImpl implements DirectionController {
 
@@ -13,13 +15,13 @@ public class DirectionControllerImpl implements DirectionController {
     private String apiKey;
 
     @Override
-    public String getDirections(@RequestParam(value="origin", defaultValue="Disneyland") String origin,
-                                @RequestParam(value="destination", defaultValue="Universal Studios Hollywood") String destination) {
+    public String getDirections(String origin, String mode, String destination) {
         // Construire l'URL pour l'appel à l'API Google Directions
         String url = "https://maps.googleapis.com/maps/api/directions/json?origin="
-                + origin + "&destination=" + destination + "&key=" + apiKey;
+                + origin + "&mode=" + mode + "&destination=" + destination + "&key=" + apiKey;
 
         RestTemplate restTemplate = new RestTemplate();
+        log.info("GET " + url);
         // Effectuer la requête GET et retourner la réponse JSON
         return restTemplate.getForObject(url, String.class);
     }
